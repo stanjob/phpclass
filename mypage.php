7682309ada9537ee80a950b952f67389eeade6cb
stan <drivesafe24@myyahoo.com>
2025-06-23 13:21:55 +0800

    phpclass

---
+++ mypage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>firstphp</title>
+</head>
+<body>
+    <?php
+    echo "welcome to my first php page! <br>";
+	echo "welcome to my first php page!","test1","test2<br>";
+	
+	
+	//print can only print one variable
+	print "welcome to my first php page! by print";
+		
+	echo 'Hello','world'; //can use multiple variable
+	echo "<br>";
+	print 'hello'; //only one variable, if there is more, there will be error
+	echo "<br>";
+	echo print "printout and input";
+	echo "<br>";
+	//output
+	echo "Hello world!";
+	echo"<br>";
+	print "Hello World!";
+	
+    ?>
+
+    
+    
+    
+</body>
+</html>
